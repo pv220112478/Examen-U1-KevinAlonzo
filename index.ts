436d98f9a518825fd322c5e70afd0673a6fa1f81
pv220112478 <pv220112478@vallarta.tecmm.edu.mx>
2026-02-18 17:24:18 -0600

    creacion de variables y si esta abierto o cerrada la cafeteria

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("holamundo")
+let cafe: number = 0x1EB;
+let azucar: number = 0xDE;
+let leche: number = 0x7B;
+let hielo: number = 0x142;
+let chocolate: number = 0x63; 
+console.log(cafe)
+console.log(azucar)
+console.log(leche)
+console.log(hielo)
+console.log(chocolate)
+let cafeteria: boolean = true;
+
+if (cafeteria=true){
+    console.log("abierto")
+
+}else{
+    console.log("cerrado")
+}
